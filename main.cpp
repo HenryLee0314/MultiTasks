3aa775eb131dd709bc69ce7a27142cc8e7296bcb
YihengLi <leehenry0314@me.com>
2022-02-23 01:18:21 +1300

    init

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "log.h"
+
+namespace TTF {
+
+} // namespace TTF
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    CGRA_LOGD("123");
+    return 0;
+}
